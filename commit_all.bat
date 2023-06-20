9cbf77dd70bbe3b11d6f449e9bb1b7248e0998fa
Chris Maughan <mornymorny@gmail.com>
2023-06-20 12:38:46 +0100

    latest

---
+++ commit_all.bat
@@ -0,0 +1,17 @@
+cd external
+cd zing
+cd libs
+cd zest
+git add .
+git commit -m %1
+git push
+cd ..
+cd ..
+git add .
+git commit -m %1
+git push
+cd ..
+cd ..
+git add .
+git commit -m %1
+git push
