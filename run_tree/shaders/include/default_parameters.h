b73da1341765295b695600b62efbcd2624fda025
Morny <mornymorny@gmail.com>
2023-06-21 10:23:43 +0100

    Add shadertoy params

--- run_tree/shaders/include/default_parameters.h
+++ run_tree/shaders/include/default_parameters.h
@@ -33,3 +33,4 @@ layout(set = 0, binding = 0) uniform UBO
     
 
 } ubo; 
+
---
+++ run_tree/shaders/include/shadertoy_parameters.h
@@ -0,0 +1,19 @@
+#include "default_parameters.h"
+
+// Shadertoy shortcuts
+#define iTime ubo.iTime
+#define iResolution ubo.iResolution
+#define iGlobalTime ubo.iGlobalTime
+#define iTimeDelta ubo.iTimeDelta
+#define iFrame ubo.iFrame
+#define iFrameRate ubo.iFrameRate
+#define iSampleRate ubo.iSampleRate
+#define iResolution ubo.iResolution
+#define iMouse ubo.iMouse
+#define iDate ubo.iDate
+#define iSpectrumBands[2] ubo.iSpectrumBands
+#define iChannelTime ubo.iChannelTime
+#define iChannelResolution[4] ubo.iChannelResolution
+#define ifFragCoordOffsetUniform ubo.ifFragCoordOffsetUniform
+#define iChannel ubo.iChannel
+
